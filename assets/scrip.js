2ff73d71ff04e7d919cac5f7a605b24ce45c9436
Nassim <127806672+mziliNassim@users.noreply.github.com>
2024-03-12 22:15:50 +0000

    API Axios

---
+++ assets/scrip.js
@@ -0,0 +1,49 @@
+const url = "https://jsonplaceholder.typicode.com/posts?userId=1";
+
+getUsers().then(() => getPosts());
+
+function getUsers() {
+  return axios
+    .get(url)
+    .then((response) => {
+      let { data: users } = response;
+      let usersUI = document.querySelector(".users");
+
+      users.map((user) => {
+        usersUI.innerHTML += `
+        <div class="user" onclick="getPosts(${user.id}, this)" >
+          <div>username : ${user.name}</div>
+          <div>email : ${user.email}</div>
+        </div>
+      `;
+      });
+    })
+    .catch((err) => console.log(err));
+}
+
+function getPosts(userID = null, elem = null) {
+  let endPoint = userID ? "?userId=" + userID : "";
+  endPoint && activeElem(elem);
+  axios
+    .get(`https://jsonplaceholder.typicode.com/posts${endPoint}`)
+    .then((response) => {
+      let { data: posts } = response;
+      let postsUI = document.querySelector(".posts");
+      postsUI.innerHTML = "Number of posts: " + posts.length;
+      posts.map((post) => {
+        postsUI.innerHTML += `
+          <div class="post">
+            <div class="post_title">${post.title}</div>
+            <div class="post_body">${post.body}</div>
+          </div>
+        `;
+      });
+    });
+}
+
+function activeElem(elem) {
+  document.querySelectorAll(".active").forEach((user) => {
+    user.classList.remove("active");
+  });
+  elem.classList.add("active");
+}
